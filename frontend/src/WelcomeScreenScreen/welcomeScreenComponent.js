e936196b78e4309606074d1e3e380bd93a4c712f
Rohan Vishnoi <avirohanvishnoi@gmail.com>
2024-05-11 16:28:03 +0530

    Image added to welcome Screen

--- frontend/src/WelcomeScreenScreen/welcomeScreenComponent.js
+++ frontend/src/WelcomeScreenScreen/welcomeScreenComponent.js
@@ -1,10 +1,12 @@
+
+
 export default function WelcomeScreenComponent() {
 
     return (
         <div className="WelcomeScreen">
             <div className="content">
                 <h1>WelcomeScreen</h1>
-                <img src="pic_trulli.jpg" width="500" height="500"></img>
+                <img src="" width="500" height="500" alt="Italian Trulli"></img>
             </div>
         </div>
     )
