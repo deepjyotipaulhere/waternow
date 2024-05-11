5af79f7687f1813d2158d1dd5aade58a0f89a7cf
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2024-05-11 18:09:43 +0530

    Update welcomeScreenComponent.js

--- frontend/src/WelcomeScreenScreen/welcomeScreenComponent.js
+++ frontend/src/WelcomeScreenScreen/welcomeScreenComponent.js
@@ -1,73 +1,60 @@
+import { Link } from "react-router-dom";
 import img from "../images/futureImage.jpg";
 import logo from "../images/logo.jpg";
 import {
-  AppBar,
-  Container,
-  Toolbar,
-  Typography,
-  Box,
-  Button,
+	AppBar,
+	Container,
+	Toolbar,
+	Typography,
+	Box,
+	Button,
 } from "@mui/material";
 
 function WelcomeScreenComponent() {
-  return (
-    <>
-      <AppBar position="static" style={{ backgroundColor: "black" }}>
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              <img src={logo} width="50" height="50" alt="Italian Trulli"></img>
-            </Typography>
+	return (
+		<>
+			<AppBar position="static" style={{ backgroundColor: "black" }}>
+				<Container maxWidth="xl">
+					<Toolbar disableGutters>
+						<Typography
+							variant="h6"
+							noWrap
+							component="a"
+							href="#app-bar-with-responsive-menu"
+							sx={{
+								mr: 2,
+								display: { xs: "none", md: "flex" },
+								fontFamily: "monospace",
+								fontWeight: 700,
+								letterSpacing: ".3rem",
+								color: "inherit",
+								textDecoration: "none",
+							}}
+						>
+							<img src={logo} width="50" height="50" alt="Italian Trulli"></img>
+						</Typography>
 
-            <Box
-              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-            ></Box>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            ></Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              <Button sx={{ my: 2, color: "white", display: "block" }}>
-                <h4>WaterNow</h4>
-              </Button>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+						<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
 
-      <div className="WelcomeScreen">
-        <div className="content">
-          <img src={img} width="2688" height="1536" alt="Italian Trulli"></img>
-        </div>
-      </div>
-    </>
-  );
+						</Box>
+						<Box sx={{ display: { xs: "none", md: "flex" } }}>
+							<Link to='/login'>
+								<Button sx={{ my: 2, color: "white", display: "block" }}>
+									Login
+								</Button>
+							</Link>
+						</Box>
+					</Toolbar>
+				</Container>
+			</AppBar>
+
+			<div className="WelcomeScreen">
+				<div className="content">
+					<img src={img} width="2688" height="1536" alt="Italian Trulli"></img>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default WelcomeScreenComponent;
