d6fc0f8074cd66c6d4e6dc1d9e93f0c3993c6a40
Rohan Vishnoi <avirohanvishnoi@gmail.com>
2024-05-11 16:00:45 +0530

    intialComponent Intialization with welcom Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
-
-export default function App() {
-
-	return (
-		<div>App</div>
-	)
+import logo from './logo.svg';
+import './App.css';
+import WelcomeScreenComponent from './WelcomeScreenScreen/welcomeScreenComponent';
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+		  
+		  <WelcomeScreenComponent/>
+    </div>
+  );
 }
+
+export default App;
---
+++ frontend/src/WelcomeScreenScreen/welcomeScreenComponent.js
@@ -0,0 +1,12 @@
+export default function WelcomeScreenComponent() {
+
+    return (
+        <div className="WelcomeScreen">
+            <div className="content">
+                <h1>WelcomeScreen</h1>
+                <img src="pic_trulli.jpg" width="500" height="500"></img>
+            </div>
+        </div>
+    )
+    
+}
